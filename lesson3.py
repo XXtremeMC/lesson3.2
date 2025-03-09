6ad0540b54ad48a57faaf1822d58ffa2a181d870
XXtremeMC <vadim.yurchenko.2002@gmail.com>
2025-03-09 15:04:49 +0200

    Lesson3.2

--- lesson3.py
+++ lesson3.py
@@ -1,22 +1,4 @@
-num1 = int(input("Enter number: "))
-num2 = (input("Operator: "))
-num3 = int(input("Enter number: "))
 
-if num2 == "+":
-    result = num1 + num3
-    print(result)
-elif num2 == "-":
-    result = num1 - num3
-    print(result)
-elif num2 == "*":
-    result = num1 * num3
-    print(result)
-elif num2 == "/":
-    if num3 == 0:
-        print("Not divisible by zero")
-    else:
-        result = num1 / num3
-        print(result)
-else:
-    print("Error")
-
+lst = [1, 2, 3, 4]
+lst = lst[-1:] + lst[:-1]
+print(lst)
